ecb6e7598f42d87c9a3dd6261c39fd1727db833b
Amanda Trang <at669@cornell.edu>
2022-01-15 23:25:39 -0800

    link fix, preview

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -4,9 +4,9 @@ import styles from '../styles/ProjectCard.module.css';
 const ProjectCard = ({ project }) => {
   return (
     <div className={styles.card}>
-      <a href={project.demo}><Image src={project.image} height={300} width={600} alt={project.name} objectFit='cover' /></a>
+      <a href={project.demo} target="_blank" rel="noopener"><Image src={project.image} height={300} width={600} alt={project.name} objectFit='cover' /></a>
       <div className={styles.content}>
-        <a href={project.demo} style={{ color: 'inherit', textDecoration: 'inherit'}}><h3>{project.name}</h3></a>
+        <a href={project.demo} style={{ color: 'inherit', textDecoration: 'inherit'}} target="_blank" rel="noopener"><h3>{project.name}</h3></a>
         <p>{project.description}</p>
         <div className={styles.tags}>
           {project.tags.map((tag) => (
