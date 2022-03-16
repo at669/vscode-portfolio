c938aa0202be8cad6954349065ef9809d4ee8ec7
Amanda Trang <at669@cornell.edu>
2022-03-15 19:07:11 -0700

    Update preview

--- pages/api/preview.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    res.setPreviewData(getStaticProps)
-    res.end('Preview mode enabled')
-}
-
-export async function getStaticProps(context) {
-    const res = await fetch(`https://amandatrang.vercel.app/${context.preview ? 'preview' : ''}`)
-}
