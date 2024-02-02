5538855af95bd38e643ab8aee11fab8fa7fd93aa
Amanda <31416491+at669@users.noreply.github.com>
2024-02-02 13:39:44 -0800

    Update (#5)
    * Update
    
    * Package update
    
    * Package update
    
    * Description update
    
    * URL fix
    
    * Image resize
    
    * Update image

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,9 +5,9 @@ const AboutPage = () => {
       <br></br>
       <p>I'm a software engineer with a hardware background, and I think games are neat.</p>
       <br></br>
-      <p>I've recently received my M.Eng in Computer Science and a B.S. in Electrical and Computer Engineering from Cornell University,</p>
-      <p>with a minor in Game Design. My interests span <b>game development, VR/AR, and hardware and robotics</b>. In my spare time, I enjoy</p>
-      <p>listening to and making music, playing and making video games, falling off my skateboard, and taking low-quality pictures of dogs.</p>
+      <p>My interests span <b>game development, VR/AR, and hardware and robotics</b>. In my spare time, I enjoy</p>
+      <p>playing and making video games, listening to and making music, falling off my skateboard, </p>
+      <p>and taking low-quality pictures of dogs.</p>
     </>
   );
 };
